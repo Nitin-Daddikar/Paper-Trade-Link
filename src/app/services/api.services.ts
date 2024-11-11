7b46e5e22a27216fc91fe49d3b7d697b14384d72
Nitin Daddikar <clarus.nitin@gmail.com>
2024-11-11 22:07:41 +0530

    http to https

--- src/app/services/api.services.ts
+++ src/app/services/api.services.ts
@@ -7,7 +7,7 @@ import { UtilitiesService } from './utilities.services';
 })
 export class APIService {
 
-  apiUrl = 'http://papertradelink.com/apis/';
+  apiUrl = 'https://papertradelink.com/apis/';
   // apiUrl = 'http://test.papertradelink.com/apis/';
 
   constructor(private httpClient: HttpClient) {
